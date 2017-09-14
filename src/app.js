1a514dd0f7b73175b0c23fb2fa06de23f5c75cd7
Naresh Bhatia <naresh.archfirst@gmail.com>
2017-09-14 13:31:48 +0200

    Updated dependencies

--- src/app.js
+++ src/app.js
@@ -4,24 +4,22 @@ import blue from 'material-ui/colors/blue';
 import pink from 'material-ui/colors/pink';
 import red from 'material-ui/colors/red';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import createPalette from 'material-ui/styles/palette';
-import createMuiTheme from 'material-ui/styles/theme';
-import createTypography from 'material-ui/styles/typography';
+import createMuiTheme from 'material-ui/styles/createMuiTheme';
 import { Provider } from 'mobx-react';
 import { Shell } from './shell';
 
 export class App extends React.Component {
     render() {
-        const palette = createPalette({
+        const palette = {
             primary: blue,
-            accent: pink,
+            secondary: pink,
             error: red,
             type: 'light'
-        });
+        };
 
-        const typography = createTypography(palette, {
+        const typography = {
             fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif'
-        });
+        };
 
         const theme = createMuiTheme({ palette, typography });
 
